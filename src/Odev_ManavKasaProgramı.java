eae0cc4744801752ab329032d6e663babeb24232
Samet Yılmaz <yilmazsamet98@gmail.com>
2022-10-06 02:43:00 +0300

    ManavKasaOdevi

---
+++ src/Odev_ManavKasaProgramı.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Odev_ManavKasaProgramı {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double armut = 2.14, elma = 3.67, domates = 1.11, muz = 0.95, patlican = 5.00, toplam, kilo;
+        System.out.println("Armut kaç kilo?");
+        kilo = input.nextDouble();
+        toplam = armut * kilo;
+        System.out.println("Elma kaç kilo?");
+        kilo = input.nextDouble();
+        toplam += armut * kilo;
+        System.out.println("Domates kaç kilo?");
+        kilo = input.nextDouble();
+        toplam += armut * kilo;
+        System.out.println("Muz kaç kilo?");
+        kilo = input.nextDouble();
+        toplam += armut * kilo;
+        System.out.println("Patlıcan kaç kilo?");
+        kilo = input.nextDouble();
+        toplam += armut * kilo;
+        System.out.println("Toplam tutar:" + toplam + "TL");
+
+
+
+
+    }
+}

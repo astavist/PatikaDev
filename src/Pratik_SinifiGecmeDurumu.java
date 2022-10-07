40dd8ce012efd528e57c2e7f16a919abfc553d92
Samet Yılmaz <yilmazsamet98@gmail.com>
2022-10-07 18:31:24 +0300

    PratikKullaniciGiririsi_Guvenlik_OdevKismi_gectiKaldi

--- src/Pratik_SinifiGecmeDurumu.java
+++ src/Pratik_SinifiGecmeDurumu.java
@@ -2,6 +2,54 @@ import java.util.Scanner;
 public class Pratik_SinifiGecmeDurumu {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        int mat, fizik, turkce, kimya, muzik, toplamNot = 0, toplamDers = 0;
+        System.out.println("Matematik notunuzu giriniz:");
+        mat = input.nextInt();
+        if ((mat>100) || (mat<0)) {
+        }
+        else {
+            toplamNot += mat;
+            toplamDers++;
+        }
+        System.out.println("Fizik notunuzu giriniz:");
+        fizik = input.nextInt();
+        if ((fizik>100) || (fizik<0)) {
+        }
+        else {
+            toplamNot += fizik;
+            toplamDers++;
+        }
+        System.out.println("Türkçe notunuzu giriniz:");
+        turkce = input.nextInt();
+        if ((turkce>100) || (turkce<0)) {
+        }
+        else {
+            toplamNot += turkce;
+            toplamDers++;
+        }
+        System.out.println("Kimya notunuzu giriniz:");
+        kimya = input.nextInt();
+        if ((kimya>100) || (kimya<0)) {
+        }
+        else {
+            toplamNot += kimya;
+            toplamDers++;
+        }
+        System.out.println("Müzik notunuzu giriniz:");
+        muzik = input.nextInt();
+        if ((muzik>100) || (muzik<0)) {
+        }
+        else {
+            toplamNot += muzik;
+            toplamDers++;
+        }
+        double average;
+        System.out.println(average = toplamNot / toplamDers);
+        if (average >= 55){
+            System.out.println("Geçti");
+        }
+        else {
+            System.out.println("Kaldı");
+        }
+        }
     }
-}

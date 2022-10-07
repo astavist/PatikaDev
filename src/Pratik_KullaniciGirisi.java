27f4f38b9791eb194ec293a35ba51dddfa5f0af0
Samet Yılmaz <yilmazsamet98@gmail.com>
2022-10-07 18:10:39 +0300

    PratikKullaniciGiririsi_Guvenlik_OdevKismi

--- src/Pratik_KullaniciGirisi.java
+++ src/Pratik_KullaniciGirisi.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Pratik_KullaniciGirisi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String username, password;
+        String username, password, password2,password3;
         System.out.println("Kullanıcı adınızı giriniz:");
         username = input.nextLine();
 
@@ -15,20 +15,31 @@ public class Pratik_KullaniciGirisi {
             }
             else {
                 System.out.println("Şifreniz yanlış");
-                System.out.println("\nŞifrenizi giriniz:");
-                password = input.nextLine();
-                if(password.equals("123456789")){
-                    System.out.println("Giriş başarılı");
-                }
-                else {
-                    System.out.println("Şifreniz yanlış");
-                    System.out.println("\nŞifrenizi giriniz");
-                    if(password.equals("123456789")){
-                        System.out.println("Giriş başarılı");
-                    }
-                    else {
-                        System.out.println("Çok fazla hatalı giriş yaptınız");
-                    }
+                System.out.println("\nŞifrenizi sıfırlamak ister misiniz?:");
+                System.out.println("\n1-EVET\n2-HAYIR");
+                int secim;
+                secim = input.nextInt();
+                switch (secim){
+                    case(1):
+                        System.out.println("Yeni şifre giriniz");
+                        password2 = input.next();
+                        if (password2.equals("123456789")) {
+                            System.out.println("Eski şifrenizi girdiniz");
+                            System.out.println("Şifrenizi giriniz:");
+                            password3 = input.next();
+                            if (password3.equals("123456789")){
+                                System.out.println("Giriş başarılı");
+                            }
+                            else {
+                                System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+                            }
+                        }
+                        else {
+                            System.out.println("Yeni şifreniz oluşturuldu");
+                        }
+                        break;
+                    case(2):
+                        System.out.println("Şifre oluşturulamadı");
                 }
             }
         }
---
+++ src/Pratik_SinifiGecmeDurumu.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Pratik_SinifiGecmeDurumu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}

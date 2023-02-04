84d692339c71fde729e7bb2af782991aff65cb4f
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-02-04 18:03:43 +0300

    PratikSayilariKucuktenBuyugeSiralama

---
+++ src/Pratik_ciftSayiToplam.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pratik_ciftSayiToplam {
+    public static void main(String[] args) {
+        int sayi;
+        System.out.println("Sayıyı giriniz:");
+        Scanner input = new Scanner(System.in);
+        sayi = input.nextInt();
+        System.out.println("3 ve 4'e Tam Bölünen Sayılar:");
+        for (int i = 1; i < sayi; i++) {
+            if (i%3 == 0){
+                if (i%4 == 0) {
+                    System.out.println(i + " ");
+                }
+            }
+        }
+    }
+}

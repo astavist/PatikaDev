4c80b7c19285755b066026595013c6eb8368f032
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-02-04 18:25:23 +0300

    PratikSayilariKucuktenBuyugeSiralama

---
+++ src/Pratik_tekSayilarinToplami.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pratik_tekSayilarinToplami {
+    public static void main(String[] args) {
+        int sayi,toplam= 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n" + "Çift ve 4'e bölünebilen sayıların toplayan programa hoş geldiniz." + "\n");
+        do {
+            System.out.println("Lütfen sayıyı giriniz :");
+            sayi = input.nextInt();
+            if (sayi % 2 == 0) {
+                if (sayi % 4 == 0) {
+                    toplam += sayi;
+                }
+                System.out.println("Toplam: " + toplam);
+            }
+        }
+        while (sayi % 2 == 0);
+        System.out.println("Toplam: " + toplam);
+
+    }
+}

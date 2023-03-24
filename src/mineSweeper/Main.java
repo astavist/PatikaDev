39ac4770a431dc47feb58036f94db5c10214a183
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-03-24 22:47:24 +0300

    mayinTarlasi

---
+++ src/mineSweeper/Main.java
@@ -0,0 +1,10 @@
+package mineSweeper;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        MineSweeper mineSwepper = new MineSweeper(3,3);
+        mineSwepper.run();
+    }
+}

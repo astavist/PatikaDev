fef9371dbb635c615b786731337b412e71c94b12
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-03-24 18:48:24 +0300

    arrayOrtalama

---
+++ src/arrayOrtalama.java
@@ -0,0 +1,10 @@
+public class arrayOrtalama {
+    public static void main(String[] args) {
+        double[] numbers = {1, 2, 3, 4, 5};
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += (1/numbers[i]);
+        }
+        System.out.println(numbers.length/sum);
+    }
+}
